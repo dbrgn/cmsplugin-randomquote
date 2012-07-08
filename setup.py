d6386e94f06e9bd4d210ee8a56800b672cdfb49f
Danilo Bargen <gezuru@gmail.com>
2012-07-08 13:22:36 +0200

    README.rst as pypi long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 version = __import__('cmsplugin_randomquote').__version__
+readme = open('README.rst').read()
 
 setup(
     name = 'cmsplugin-randomquote',
     version = version,
     description = 'Random quote plugin for the django CMS.',
+    long_description=readme,
     author = 'Danilo Bargen',
     author_email = 'gezuru@gmail.com',
     url = 'http://github.com/gwrtheyrn/cmsplugin-randomquote',
