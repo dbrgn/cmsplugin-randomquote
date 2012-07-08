047800ccb532d12c1682c7361ff7ebee092d02f3
Danilo Bargen <gezuru@gmail.com>
2012-07-08 02:00:00 +0200

    Added missing MANIFEST.in

--- cmsplugin_randomquote/cms_plugins.py
+++ cmsplugin_randomquote/cms_plugins.py
@@ -7,7 +7,7 @@ from models import Quote
 class QuotePlugin(CMSPluginBase):
     """This plugin randomly renders a quote from the database."""
     name = _('Quote')
-    render_template = 'quote.html'
+    render_template = 'cmsplugin_randomquote/quote.html'
 
     def render(self, context, instance, placeholder):
         try:
